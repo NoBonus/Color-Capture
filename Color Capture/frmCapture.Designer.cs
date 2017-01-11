952bc5b4f746d6a979d5ee27a56df4d7f47285b2
NoBonus <radekdiaz@gmail.com>
2017-01-11 18:35:53 -0300

    Agregar archivos de proyecto.

---
+++ Color Capture/frmCapture.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Color_Capture
+{
+    partial class frmCapture
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.txtHtmlColor = new System.Windows.Forms.TextBox();
+            this.pnlSelColor = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCapture.Location = new System.Drawing.Point(12, 12);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(24, 23);
+            this.btnCapture.TabIndex = 0;
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // txtHtmlColor
+            // 
+            this.txtHtmlColor.Location = new System.Drawing.Point(75, 12);
+            this.txtHtmlColor.Name = "txtHtmlColor";
+            this.txtHtmlColor.ReadOnly = true;
+            this.txtHtmlColor.Size = new System.Drawing.Size(197, 20);
+            this.txtHtmlColor.TabIndex = 1;
+            this.txtHtmlColor.TabStop = false;
+            // 
+            // pnlSelColor
+            // 
+            this.pnlSelColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSelColor.Location = new System.Drawing.Point(42, 12);
+            this.pnlSelColor.Name = "pnlSelColor";
+            this.pnlSelColor.Size = new System.Drawing.Size(27, 23);
+            this.pnlSelColor.TabIndex = 2;
+            // 
+            // frmCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pnlSelColor);
+            this.Controls.Add(this.txtHtmlColor);
+            this.Controls.Add(this.btnCapture);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmCapture";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.TextBox txtHtmlColor;
+        private System.Windows.Forms.Panel pnlSelColor;
+    }
+}
+
---
+++ Color Capture/frmCapture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Color_Capture
+{
+    public partial class frmCapture : Form
+    {
+        public frmCapture()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            MouseHook.Start();
+            MouseHook.MouseAction += MouseHook_MouseAction;
+        }
+
+        private void MouseHook_MouseAction(object sender, MouseHook.RawMouseEventArgs e)
+        {
+            Console.WriteLine("mouse act" + e.Message);
+            if (e.Message == MouseHook.MouseMessages.WM_LBUTTONDOWN)
+            {
+                Console.WriteLine("mouse click "+e.Point.x+":"+e.Point.y);
+                MouseHook.Stop();
+                MouseHook.MouseAction -= MouseHook_MouseAction;
+                Color clcolor = GetColorAt(e.Point.x, e.Point.y);
+                pnlSelColor.BackColor = clcolor;
+                string htmlcol = ColorTranslator.ToHtml(Color.FromArgb(clcolor.ToArgb()));
+                txtHtmlColor.Text = htmlcol;
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+    }
+}
